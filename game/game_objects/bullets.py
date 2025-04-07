30873bedf9ba3be9a00eacbc4da231fd8866ac01
FelipeFerraz4 <felipeferraz2402@gmail.com>
2025-04-07 20:30:33 -0300

    feat: created bullets.py

---
+++ game/game_objects/bullets.py
@@ -0,0 +1,22 @@
+import pygame
+from game.assets import bullet_img, explosion_fx
+from game.game_objects.alien import alien_group
+from game.game_objects.explosion import Explosion
+from settings import explosion_group
+        
+class Bullets(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = bullet_img
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+    
+    def update(self):
+        self.rect.y -= 5
+        if self.rect.bottom < 0:
+            self.kill()
+        if pygame.sprite.spritecollide(self, alien_group, True):
+            self.kill()
+            explosion_fx.play()
+            explosion = Explosion(self.rect.centerx, self.rect.centery, 2)
+            explosion_group.add(explosion)
